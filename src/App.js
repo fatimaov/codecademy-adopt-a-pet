567caa80466d47270c92dfd7bcc76e0856f2c979
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-03-10 17:48:32 +0100

    feat: add dynamic routes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } 
 const appRouter = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Root />}>
     <Route index element={<HomePage />} />
-  </Route >
+    <Route path=':type' element={<HomePage />} />
+    <Route path=':type/:id' element={<PetDetailsPage />} />
+  </Route>
 ));
 
 function App() {
